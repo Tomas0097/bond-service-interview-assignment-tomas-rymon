582caa3df7b8a570f83ecbf591f39db27699c129
Tomas Rymon <tomas.rymon@gmail.com>
2024-07-21 15:25:40 +0200

    commit

---
+++ src/web/static/dist/js/build.js
@@ -0,0 +1,33 @@
+class BondServiceInterviewAssignmentClient {
+    baseUrl = 'http://127.0.0.1:8088/api/'
+
+    sendRequest(endPoint, method, data, resultHandler) {
+        fetch(this.baseUrl + endPoint, {
+            method: method,
+            headers: {'Content-Type': 'application/json', 'X-CSRFToken': csrfToken},
+            body: JSON.stringify(data),
+        })
+        .then(response => {
+            response.json().then(data => {resultHandler(response.status, data)})
+        })
+    }
+
+    submitLoginForm() {
+        const loginForm = document.getElementById('login-form');
+        const inputUsername = loginForm.querySelector('input[name="username"]').value;
+        const inputPassword = loginForm.querySelector('input[name="password"]').value;
+        const formData = {username: inputUsername, password: inputPassword};
+
+        function loginResultHandler(responseStatus, data) {
+            if (responseStatus === 200) {
+                window.location.href = 'test';
+            } else {
+                alert(data.error);
+            }
+        }
+
+        this.sendRequest('user-login/', 'POST', formData, loginResultHandler);
+    }
+};
+
+const bondServiceInterviewAssignmentClient = new BondServiceInterviewAssignmentClient();

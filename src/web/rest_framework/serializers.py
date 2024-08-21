09574af8854dd804005394bde0d23dbdcedd055a
Tomas Rymon <tomas.rymon@gmail.com>
2024-08-21 22:04:08 +0200

    Adds isin validation

--- src/web/rest_framework/serializers.py
+++ src/web/rest_framework/serializers.py
@@ -1,3 +1,4 @@
+import requests
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
@@ -15,33 +16,48 @@ class BondSerializer(serializers.ModelSerializer):
     coupon_type = CouponTypeField(BondModel.CouponType.choices)
 
     def validate(self, data):
-        coupon_type = data.get('coupon_type')
-        interest_rate = data.get('interest_rate')
-        coupon_frequency_in_months = data.get('coupon_frequency_in_months')
-        purchase_date = data.get('purchase_date')
-        maturity_date = data.get('maturity_date')
+        isin = data.get("isin")
+        coupon_type = data.get("coupon_type")
+        interest_rate = data.get("interest_rate")
+        coupon_frequency_in_months = data.get("coupon_frequency_in_months")
+        purchase_date = data.get("purchase_date")
+        maturity_date = data.get("maturity_date")
 
         if coupon_type == BondModel.CouponType.ZERO_COUPON:
             if interest_rate:
                 raise serializers.ValidationError({
-                    'interest_rate': 'Interest rate must be zero for zero coupon bond.'
+                    "interest_rate": "Interest rate must be zero for zero coupon bond."
                 })
             if coupon_frequency_in_months:
                 raise serializers.ValidationError({
-                    'coupon_frequency_in_months': 'Coupon frequency in months must be zero for zero coupon bond.'
+                    "coupon_frequency_in_months": "Coupon frequency in months must be zero for zero coupon bond."
                 })
         else:
             if not interest_rate:
                 raise serializers.ValidationError({
-                    'interest_rate': 'Interest rate is required for bond with coupons.'
+                    "interest_rate": "Interest rate is required for bond with coupons."
                 })
             if not coupon_frequency_in_months:
                 raise serializers.ValidationError({
-                    'coupon_frequency_in_months': 'Coupon frequency in months is required for bond with coupons.'
+                    "coupon_frequency_in_months": "Coupon frequency in months is required for bond with coupons."
                 })
 
         if maturity_date <= purchase_date:
             raise serializers.ValidationError("Purchase date must be earlier than Maturity date.")
+
+        # Check if the ISIN is valid by calling central securities depository API
+        response = requests.get(f"https://www.cdcp.cz/isbpublicjson/api/VydaneISINy?isin={isin}")
+
+        if response.status_code == 200:
+            pass
+        elif response.status_code == 400:
+            raise serializers.ValidationError({
+                "isin": "The ISIN is not valid by the central securities depository."
+            })
+        else:
+            raise serializers.ValidationError({
+                "isin": "The central securities depository failed with validating the ISIN."
+            })
 
         return data
 
